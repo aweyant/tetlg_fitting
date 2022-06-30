a4cf6b31b0a7793a03095859b25c8b0f3322a53e
aweyant <aweyant@ucsd.edu>
2022-06-30 13:25:25 -0700

    Peaking at the time dimension of the .nc files

---
+++ scratch_correct_time_dimension.R
@@ -0,0 +1,7 @@
+# Load packages -----------------------------------------------------------
+library(tidync)
+
+# Load file ----------------------------------------------------------------
+prec_1915_nc <-tidync("./data/livneh_unsplit/prec.1915.nc")
+
+prec_1915_nc$variable
