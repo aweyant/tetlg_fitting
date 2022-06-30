b21fe7fa1a13701edc24e90d703a86b1ebcf4917
aweyant <aweyant@ucsd.edu>
2022-06-30 10:35:08 -0700

    Created experimental version of create_events() which allows for local (grid-scale) quantile thresholds

---
+++ test_create_events.R
@@ -0,0 +1,13 @@
+# Load packages -----------------------------------------------------------
+library(tidync)
+
+# Load needed functions ---------------------------------------------------
+source(file = "fun_create_events.R")
+
+# Load and wrangle test file ----------------------------------------------
+
+
+# Apply create_events function --------------------------------------------
+
+
+
